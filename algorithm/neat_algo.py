2a01d69dfa87f3aaa2e9d144d0b06fb63dbc4008
Jodhan Gill <jga145@sfu.ca>
2024-05-27 18:03:13 -0700

    Added NEAT example

---
+++ algorithm/neat_algo.py
@@ -0,0 +1,46 @@
+import neat
+
+xor_inputs = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)]
+xor_outputs = [(0.0,), (1.0,), (1.0,), (0.0,)]
+
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        genome.fitness = 4.0
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        for xi, xo in zip(xor_inputs, xor_outputs):
+            output = net.activate(xi)
+            genome.fitness -= (output[0] - xo[0]) ** 2
+
+def run(config_file):
+    # Load configuration.
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
+
+    # Create the population, which is the top-level object for a NEAT run.
+    p = neat.Population(config)
+
+    # Add a stdout reporter to show progress in the terminal.
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    # Run for up to 300 generations.
+    winner = p.run(eval_genomes, 300)
+
+    # Display the winning genome.
+    print('\nBest genome:\n{!s}'.format(winner))
+
+    # Show output of the most fit genome against training data.
+    print('\nOutput:')
+    winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+    for xi, xo in zip(xor_inputs, xor_outputs):
+        output = winner_net.activate(xi)
+        print("input {!r}, expected output {!r}, got {!r}".format(xi, xo, output))
+
+    node_names = {-1: 'A', -2: 'B', 0: 'A XOR B'}
+
+
+
+if __name__ == '__main__':
+    run('neat_config.ini')

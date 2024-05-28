0384b86735aae2736d68a8f2c8f6031092e93f78
Jodhan Gill <jga145@sfu.ca>
2024-05-27 18:04:14 -0700

    Added config parsing functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from algorithm import config_parser
 
 app = Flask(__name__)
 
@@ -9,10 +10,17 @@ def index():
 # Handle incoming message
 @app.route('/process', methods=['POST'])
 def process():
-    message = request.form.get('message')
-    print(message)
-
-    return jsonify({'message': message})
+    data = request.form.to_dict()
+    
+    # Parse boolean values correctly
+    data['reset_on_extinction'] = data.get('reset_on_extinction') == 'on'
+    data['enabled_default'] = data.get('enabled_default') == 'on'
+    data['feed_forward'] = data.get('feed_forward') == 'on'
+    
+    # Create config file and get the filename and message
+    result =  config_parser.process_parameters(data)
+    
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)

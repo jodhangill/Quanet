4ac84c86c939cdc23d2b193037969b23014fff73
Jodhan Gill <jga145@sfu.ca>
2024-06-03 19:10:17 -0700

    Implement basic textbox features for fitness calculator

--- static/js/calculator.js
+++ static/js/calculator.js
@@ -1,3 +1,6 @@
+let fitnessFunc = "";
+let textWidths = [];
+
 
 function setDisplaySize() {
     // Set height of calculator display based on other elements
@@ -8,8 +11,48 @@ function setDisplaySize() {
     displayContainer.style.height = `${window.innerHeight - calcPad.offsetHeight - homeButton.offsetHeight - 36}px`;
 }
 
+function handleClick(event) {
+    const id = event.target.id
+
+    // Track the current function text
+    if (id == 'backspace') {
+        fitnessFunc = fitnessFunc.substring(0, fitnessFunc.length - 1);
+    }
+    else if (id == 'clear') {
+        fitnessFunc = "";
+    }
+    else if (id == 'done') {
+        window.location.href = '/configurator';
+    }
+    else {
+        fitnessFunc += event.target.innerText;
+    }
+
+    // Display fitness function
+    const output = document.getElementById('output');
+    output.innerText = fitnessFunc;
+
+    // Adjust caret position
+    const outputFrame = output.getBoundingClientRect();
+    const caret = document.getElementById('caret');
+    caret.style.top = outputFrame.top - 90 + 'px';
+    caret.style.left = outputFrame.right - 16 + 'px';
+}
+
+function setClickHandlers() {
+    const buttonContainer = document.getElementById('keyboard');
+
+    // Get all calculator buttons
+    const buttons = buttonContainer.getElementsByTagName('button');
+
+        for (let i = 0; i < buttons.length; i++) {
+        buttons[i].onclick = handleClick;
+    }
+}
+
 window.onload = function () {
     setDisplaySize();
+    setClickHandlers();
 };
 window.addEventListener('resize', function(event) {
     setDisplaySize();
